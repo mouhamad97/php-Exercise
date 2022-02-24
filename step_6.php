468095048180c9b61f23983c59c41a3a2cc060bb
mouhamad haydar <mouhamadhaydar052@gmail.com>
2022-02-24 15:53:34 +0200

    step 7

---
+++ step_6.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+function calculateArea($height, $width){
+
+echo $height * $width;
+
+}
+
+echo calculateArea(5,5);
+
+?>
---
+++ step_7.php
@@ -0,0 +1,13 @@
+<?php
+
+$birth_year = 2012;
+$current_year = 2022;
+ 
+if(($current_year - $birth_year) > 18){
+    echo 'You can drive';
+
+} else{
+    echo 'You still a kid, go and play GTA';
+}  
+
+?>
