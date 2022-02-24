66dac5d2b03e2ed55e85302c21e495d0afdf68da
mouhamad haydar <mouhamadhaydar052@gmail.com>
2022-02-24 16:15:29 +0200

    step 8

---
+++ Step_8.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+
+$grocery = ['Eggs','Milk','Cheese','Water Pack','Tissues','Watermelon'];
+
+echo "Hello sir , do you have $grocery[0] , $gorcery[1] and $gorcery[2] ? also if you sell fruits can i find a $grocery[5]";
+
+?>
