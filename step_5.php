aa77f7f606838b94f3f7afe9d962c7cf01f0dd8c
mouhamad haydar <mouhamadhaydar052@gmail.com>
2022-02-24 15:50:04 +0200

    step 6

---
+++ step_5.php
@@ -0,0 +1,14 @@
+<?php
+
+$course_name = 'web';
+$enrolled_students = 2;
+$price= 110;
+$on_discount = true; 
+
+$hasDiscount =  $on_discount ? 'yes' : 'no';
+
+
+echo "course title: $course_name \n  enrolled students: $enrolled_students \n course price: $price $ USD \n course on discound: $hasDiscount"
+
+
+?>
