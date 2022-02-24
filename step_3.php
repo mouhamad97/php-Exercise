f097e3c8573a90b31cfb19e756a9a7732aea7f11
mouhamad haydar <mouhamadhaydar052@gmail.com>
2022-02-24 12:02:47 +0200

    step3

---
+++ step_3.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "mohammed haydar";
+$email = "mouhamadhaydar052@gmail.com";
+
+echo 'My name is '. $name .' and my email is '. $email.' ';
+
+$height = 180;
+$width =30;
+$area = $height * $width;
+echo  $area;
+
+?>
