c113db9c28f47468b9dc6a6d29da78f7a7da2fa7
mouhamad haydar <mouhamadhaydar052@gmail.com>
2022-02-24 14:58:10 +0200

    step4 fixed

--- step_4.php
+++ step_4.php
@@ -8,7 +8,7 @@ echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$g
 
 echo "Hello, I'm $name, I'm a $position please check my github link $github_url \n";
 
-echo 'Hello, I\'m $name, I\'m a $position please check my github link $github_url ';
+echo 'Hello, I\'m '.$name.', I\'m a '.$position.' please check my github link '.$github_url.'';
 
 
 ?>
