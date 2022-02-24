1b6012ae95247845a115231036a40c5f9582fa6e
mouhamad haydar <mouhamadhaydar052@gmail.com>
2022-02-24 14:53:33 +0200

    step4

---
+++ step_4.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = 'mohammed haydar';
+$position = 'Web developer';
+$github_url ='https://github.com/mouhamad97';
+
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url." \n";
+
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url \n";
+
+echo 'Hello, I\'m $name, I\'m a $position please check my github link $github_url ';
+
+
+?>
