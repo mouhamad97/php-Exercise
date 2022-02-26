f1a48e6119f78ac15ce94224a2b664fa3fa4edcd
mouhamad haydar <mouhamadhaydar052@gmail.com>
2022-02-26 22:34:47 +0200

    basic-02 step 5

---
+++ Basic-02/step_5.php
@@ -0,0 +1,44 @@
+<?php 
+
+switch (5) {
+    case 0:
+        echo 'zero';
+      break;
+    case 1:
+      echo 'one';
+      break;
+      case 2:
+        echo 'two';
+        break;
+        case 3:
+            echo 'tjre';
+            break;
+            case 4:
+                echo 'four';
+                break;
+                case 5:
+                    echo 'five';
+                    break;
+                    case 6:
+                        echo 'six';
+                        break;
+                        case 7:
+                            echo 'seven';
+                            break;
+                            case 8:
+                                echo 'eight';
+                                break;
+                                case 9:
+                                    echo 'nine';
+                                    break;
+                                    case 10:
+                                        echo 'ten';
+                                        break;
+    
+  
+       
+  }
+
+
+
+?>
